05e4b026f00d7fe36770ce17c61f113eb85ffd8e
Mark Kenneth M. Bayona <mkmbayona@gmail.com>
2021-07-26 18:18:31 +0800

    Tip change action and calculate button action Default should 20% tip

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -10,19 +10,31 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
+    var tip: Float = 0.20
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
+    @IBOutlet var tipButtons: [UIButton]!
+    
     @IBAction func tipChanged(_ sender: UIButton) {
+        for btn in tipButtons {
+            btn.isSelected = btn == sender
+        }
+        
+        let tipString = sender.currentTitle?.dropLast() ?? "20"
+        
+        tip = Float(tipString)! / 100
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        print(tip)
     }
 
 }
